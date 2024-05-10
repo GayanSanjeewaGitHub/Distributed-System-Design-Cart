5fa6ee3b9f00a149bb8a0a6436d4375042f47079
Gayaneng <139423112+Gayaneng@users.noreply.github.com>
2024-05-10 22:29:04 +0530

    add

--- seller/src/main/java/ds/tutorial/communication/seller/SellerServiceClient.java
+++ seller/src/main/java/ds/tutorial/communication/seller/SellerServiceClient.java
@@ -18,11 +18,14 @@ class SellerServiceClient {
 
     LoginServiceGrpc.LoginServiceBlockingStub sellerLoginStub = null;
     LogoutServiceGrpc.LogoutServiceBlockingStub sellerLogoutStub = null;
-    AddItemServiceGrpc.AddItemServiceBlockingStub addItemServiceBlockingStub = null;
+    AddItemServiceGrpc.AddItemServiceBlockingStub addItemStub = null;
+
+    DeleteItemServiceGrpc.DeleteItemServiceBlockingStub deleteItemStub = null;
     String host = null;
     int port = -1;
     String mode;
     private ManagedChannel channel = null;
+
     public SellerServiceClient(String host, int port, String mode) {
         this.host = host;
         this.port = port;
@@ -183,8 +186,8 @@ class SellerServiceClient {
 
         SellerServiceClient client = new SellerServiceClient(host, port, mode);
         client.initializeConnection();
-       client.processUserRequests();
-          client.closeConnection();
+        client.processUserRequests();
+        client.closeConnection();
 
     }
 
@@ -194,7 +197,7 @@ class SellerServiceClient {
         channel = ManagedChannelBuilder.forAddress("localhost", port).usePlaintext().build();
         sellerLoginStub = LoginServiceGrpc.newBlockingStub(channel);
         sellerLogoutStub = LogoutServiceGrpc.newBlockingStub(channel);
-        addItemServiceBlockingStub = AddItemServiceGrpc.newBlockingStub(channel);
+        addItemStub = AddItemServiceGrpc.newBlockingStub(channel);
 
     }
 
@@ -202,103 +205,84 @@ class SellerServiceClient {
         channel.shutdown();
     }
 
-     private void processUserRequests() throws InterruptedException {
+    private void processUserRequests() throws InterruptedException {
 
-         while (true) {
-             if (mode.equals("login")) {
-                 Scanner userInput = new Scanner(System.in);
-                 System.out.println("\nEnter Seller Account ID , password to login :");
-                 String sellerId = userInput.nextLine().trim();
-                 String password = userInput.nextLine().trim();
-                 System.out.println("Requesting server to log in user  " + sellerId.toString());
-                 LoggingRequest loginrequest = LoggingRequest
-                         .newBuilder()
-                         .setAccountId(sellerId)
-                         .setPassword(password)
-                         .build();
-                 LoggingResponse loginresponse = sellerLoginStub.login(loginrequest);
-                 if(loginresponse.getStatus() == true){
-                     System.out.printf(" Login Success for  sellerId " + sellerId  );
-                 }else{
-                     System.out.printf(" Login not Success for  sellerId " + sellerId  );
-                 }
-                 Thread.sleep(1000);
+        while (true) {
+            if (mode.equals("login")) {
+                Scanner userInput = new Scanner(System.in);
+                System.out.println("\nEnter Seller Account ID , password to login :");
+                String sellerId = userInput.nextLine().trim();
+                String password = userInput.nextLine().trim();
+                System.out.println("Requesting server to log in user  " + sellerId);
+                LoggingRequest loginrequest = LoggingRequest.newBuilder().setAccountId(sellerId).setPassword(password).build();
+                LoggingResponse loginresponse = sellerLoginStub.login(loginrequest);
+                if (loginresponse.getStatus()) {
+                    System.out.printf(" Login Success for  sellerId " + sellerId);
+                } else {
+                    System.out.printf(" Login not Success for  sellerId " + sellerId);
+                }
+                Thread.sleep(1000);
 
-             } else if (mode.equals("logout")) {
-                 Scanner userInput = new Scanner(System.in);
-                 System.out.println("\nEnter Seller Account ,  to logout :");
-                 String sellerId = userInput.nextLine().trim();
-                 System.out.println("Requesting server to log out user  " + sellerId.toString());
+            } else if (mode.equals("logout")) {
+                Scanner userInput = new Scanner(System.in);
+                System.out.println("\nEnter Seller Account ,  to logout :");
+                String sellerId = userInput.nextLine().trim();
+                System.out.println("Requesting server to log out user  " + sellerId);
 
-                 LogoutRequest sellerlogoutrequest = LogoutRequest
-                         .newBuilder()
-                         .setAccountId(sellerId)
-                         .build();
-                 LogoutResponse logoutresponse = sellerLogoutStub.logout(sellerlogoutrequest);
-                 if(logoutresponse.getStatus() == true){
-                     System.out.printf(" Logout Success for sellerId " + sellerId  );
-                 }else{
-                     System.out.printf(" Login not Success for  sellerId " + sellerId  );
-                 }
-                 Thread.sleep(1000);
+                LogoutRequest sellerlogoutrequest = LogoutRequest.newBuilder().setAccountId(sellerId).build();
+                LogoutResponse logoutresponse = sellerLogoutStub.logout(sellerlogoutrequest);
+                if (logoutresponse.getStatus()) {
+                    System.out.printf(" Logout Success for sellerId " + sellerId);
+                } else {
+                    System.out.printf(" Login not Success for  sellerId " + sellerId);
+                }
+                Thread.sleep(1000);
 
-             } else if (mode.equals("additem")) {
-                 Scanner userInput = new Scanner(System.in);
-                 System.out.println("\nEnter Seller Account , ItemId , amount , sellOrBuy , status  to Add or  Update the Item :");
-                 String sellerId = userInput.nextLine().trim();
-                 String ItemId = userInput.nextLine().trim();
-                 String ItemId = userInput.nextLine().trim();
-                 String ItemId = userInput.nextLine().trim();
-                 String ItemId = userInput.nextLine().trim();
+            } else if (mode.equals("additem")) {
+                Scanner userInput = new Scanner(System.in);
+                System.out.println("\nEnter Seller Account , ItemId , amount , sellOrBuy , status  to Add or  Update the Item :");
+                String sellerId = userInput.nextLine().trim();
+                String itemId = userInput.nextLine().trim();
+                String amount = userInput.nextLine().trim();
+                String sellorbuy = userInput.nextLine().trim();
+                String status = userInput.nextLine().trim();
 
-                 System.out.println("Requesting server to Add Item :" + ItemId.toString() + "For Seller Account " + sellerId.toString());
+                System.out.println("Requesting server to add item  " + itemId + "For seller  id " + sellerId);
 
-             } else if (mode.equals("delete")) {
-                 Scanner userInput = new Scanner(System.in);
-                 System.out.println("\nEnter Seller Account , Item Id  to delete :");
-                 String sellerId = userInput.nextLine().trim();
-                 String ItemId = userInput.nextLine().trim();
-                 System.out.println("Requesting server to delete  Item  " + ItemId.toString() + " for seller account " + sellerId.toString());
+                AddItemRequest addItemRequest = AddItemRequest.newBuilder().setSellerId(sellerId).setItemId(itemId).setAmount(Double.parseDouble(amount)).setSellOrBuy(Boolean.valueOf(sellorbuy)).setStatus(Boolean.valueOf(status)).build();
 
-             }
+                AddItemResponse addItemResponse = addItemStub.addItem(addItemRequest);
 
 
-             if (mode.equals("c")) {
+                if (addItemResponse.getStatus()) {
+                    System.out.printf(" Add Item  Success for sellerId " + sellerId + "and Item id" + itemId);
+                } else {
+                    System.out.printf(" Add Item  not Success for sellerId " + sellerId + "and Item id" + itemId);
+                }
+                Thread.sleep(1000);
 
-                 Scanner userInput = new Scanner(System.in);
-                 System.out.println("\nEnter Account ID to check the balance :");
 
-                 String accountId = userInput.nextLine().trim();
-                 System.out.println("Requesting server to check the account balance for " + accountId.toString());
-                 CheckBalanceRequest request = CheckBalanceRequest
-                         .newBuilder()
-                         .setAccountId(accountId)
-                         .build();
+            } else if (mode.equals("delete")) {
+                Scanner userInput = new Scanner(System.in);
+                System.out.println("\nEnter Seller Account , Item Id  to delete :");
+                String sellerId = userInput.nextLine().trim();
+                String itemId = userInput.nextLine().trim();
+                System.out.println("Requesting server to delete  Item  " + itemId.toString() + " for seller account " + sellerId);
 
-                 CheckBalanceResponse response = clientStub.checkBalance(request);
-                 System.out.printf("My balance is " + response.getBalance() + " LKR");
+                DeleteItemRequest deleteItemRequest = DeleteItemRequest.newBuilder().setSellerId(sellerId).setItemId(itemId).build();
 
-                 Thread.sleep(1000);
-             } else {
-                 Scanner userInput = new Scanner(System.in);
-                 System.out.println("\nEnter Account ID,amount to set the balance :");
+                DeleteItemResponse deleteItemResponse = deleteItemStub.deleteItem(deleteItemRequest);
+                if (deleteItemResponse.getStatus()) {
+                    System.out.printf(" Delete Item  Success for sellerId " + sellerId + "and Item id" + itemId);
+                } else {
+                    System.out.printf(" Delete Item  not Success for sellerId " + sellerId + "and Item id" + itemId);
+                }
+                Thread.sleep(1000);
 
-                 String setBalanceInput = userInput.nextLine().trim();
-                 String accountId = setBalanceInput.split(",")[0];
-                 double amount = Double.parseDouble(setBalanceInput.split(",")[1]);
-                 System.out.println("Requesting server to set the account balance for " + accountId.toString() + " as " + amount + " LKR");
-                 SetBalanceRequest request = SetBalanceRequest
-                         .newBuilder()
-                         .setAccountId(accountId)
-                         .setValue(amount)
-                         .build();
 
-                 SetBalanceResponse response = setBalanceClient.setBalance(request);
-                 System.out.printf("Set balance request status is " + response.getStatus());
+            }
 
-                 Thread.sleep(1000);
-             }
-         }
-     }
+        }
+    }
 }
 
